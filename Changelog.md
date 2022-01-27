e6e8690043a892a93157800939306a98d56008fb
Lanre <lanre@krikey.com>
2022-01-26 22:06:24 -0500

    0.0.4

--- Changelog.md
+++ Changelog.md
@@ -1,2 +1,5 @@
+## 0.0.4
++ Removed `features` and `types` subpackages.
+
 ## 0.0.3
 + Added NatML CLI.
--- natml/version.py
+++ natml/version.py
@@ -3,4 +3,4 @@
 #   Copyright (c) 2022 Yusuf Olokoba.
 #
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
